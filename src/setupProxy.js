d3dc9e7ea14b9a7aef1d8cb24f8f162e5ef3bd8b
Aomi <aomijokoji@gmail.com>
2022-01-15 15:24:29 -0800

    chore: react scripts and proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,13 @@
+/* eslint-disable @typescript-eslint/no-var-requires */
+const { createProxyMiddleware } = require("http-proxy-middleware");
+
+module.exports = function (app) {
+  app.use(
+    "/socket.io",
+    createProxyMiddleware({
+      target: "http://localhost:3001",
+      changeOrigin: true,
+      ws: true,
+    })
+  );
+};
