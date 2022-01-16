d81ad0b4c5691a5ee12b60c84293ac2680adef12
Keith <khr8771@gmail.com>
2022-01-16 00:50:58 -0800

    add piles in backend via frontend. goodnight:)

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,13 @@ import React, { useEffect, useState } from "react";
 import { Lobby } from "./pages/Lobby";
 import { Game } from "./api/Game";
 import { useSocket } from "./contexts/provider";
-import { Box } from "@chakra-ui/react";
+import { Box, Spinner } from "@chakra-ui/react";
 import { Route, Routes, useNavigate } from "react-router-dom";
 import { Home } from "./pages/Home";
 
 export function App() {
   const [game, setGame] = useState<Game | null>(null);
-  const { data: gameData } = useSocket<Game>("lobby:update");
+  const { data: gameData } = useSocket<Game>("clientUpdate");
 
   const navigate = useNavigate();
 
@@ -27,13 +27,7 @@ export function App() {
         <Route path="/join/:code" element={<Home />} />
         <Route
           path="/lobby/:code"
-          element={
-            <Lobby
-              game={game}
-              gameName={game?.name ?? ""}
-              players={game?.players.map((p) => p.name) ?? []}
-            />
-          }
+          element={game ? <Lobby game={game} /> : <Spinner />}
         />
       </Routes>
     </Box>
