e8cdc96c853929593fa2f22e3d6d649d6a59fc2b
Aomi <aomijokoji@gmail.com>
2022-01-16 02:32:56 -0800

    socket config

--- src/contexts/socket.ts
+++ src/contexts/socket.ts
@@ -1,8 +1,8 @@
-import React, { createContext } from "react";
+import { createContext } from "react";
 import { io } from "socket.io-client";
 
 // set your ngrok here
-const ngrok = "https://ab02-2604-3d08-2d80-a700-00-6bf2.ngrok.io";
+const ngrok = "https://ab02-2604-3d08-2d80-a700-00-6bf2.ngrok.io/";
 
 export const socket = io(process.env.NODE_ENV === "production" ? "/" : ngrok);
 
